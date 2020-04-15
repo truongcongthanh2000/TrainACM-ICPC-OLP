960b7277a1f41c5b46a56aaf6117d1172bfbb707
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2020-04-15 20:34:50 +0700

    add File Samara2020_CFGyms

---
+++ Samara2020_CFGyms/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INP "solve"
+#define OUT "solve"
+
+const int QQ = 3e5 + 10;
+const int maxN = 3e5 + 10;
+const int maxA = 1e6 + 10;
+const long long oo = 1e16;
+const int INF = 1e9 + 100;
+const int MOD = 1e9 + 7;
+const int Base = 30;
+const long double du = 1e-9;
+const string YES = "YES";
+const string NO = "NO";
+mt19937 rnd(chrono::system_clock::now().time_since_epoch().count());
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+void doc() {
+    freopen(INP ".inp","r",stdin);
+    freopen(OUT ".out","w",stdout);
+}
+
+void sol() {
+    int n, t;
+    cin >> n >> t;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (abs(a[i]) > t) continue;
+        int d = abs(a[i]);
+        int lim_x = a[i] + (t - d);
+        int j = lower_bound(a.begin(), a.end(), lim_x + 1) - a.begin();
+        res = max(res, j - i);
+    }
+    for (int i = 0; i < n; i++) {
+        if (abs(a[i]) > t) continue;
+        int d = abs(a[i]);
+        int lim_x = a[i] - (t - d);
+        int j = lower_bound(a.begin(), a.end(), lim_x) - a.begin();
+        res = max(res, i - j + 1);
+    }
+    cout << res;
+}
+
+void solve() {
+    int T;
+    ///cin >> T;
+    T = 1;
+    int TestCase = 0;
+    while (T--) {
+        TestCase += 1;
+        ///cout << "Case #" << TestCase << ": ";
+        sol();
+    }
+}
+
+int main(int argc,char *argv[]) {
+    srand(time(nullptr));
+    ios_base::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+    ///doc();
+    solve();
+}
