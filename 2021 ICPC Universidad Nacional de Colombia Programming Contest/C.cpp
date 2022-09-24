039cc56deeb4110a7447752032dd0ec09ce830d3
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2022-09-24 09:11:11 +0700

    add solution problem B, C Universidad Nacional de Comlombia

---
+++ 2021 ICPC Universidad Nacional de Colombia Programming Contest/C.cpp
@@ -0,0 +1,105 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INP "input"
+#define OUT "output"
+
+/* some template */
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& a) {
+    for (const auto& v : a) out << v << ' ';
+    out << endl;
+    return out;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<vector<T> >& a) {
+    for (const auto& v : a) {
+        for (const auto& value : v) out << value << ' ';
+        out << endl;
+    }
+    return out;
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v) {
+    for (auto& x : v) is >> x;
+    return is;
+}
+/* end template */
+
+const long long INF_LL = 1e18;
+const int INF = 1e9 + 100;
+const long double EPS = 1e-6;
+const int BLOCK = 550;
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+void open_file() {
+#ifdef THEMIS
+    freopen(INP ".txt", "r", stdin);
+    freopen(OUT ".txt", "w", stdout);
+#endif  // THEMIS
+}
+
+const int maxN = 1e6 + 100;
+const int MOD = 1e9 + 7;
+
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+void sol() {
+    string s;
+    while (cin >> s) {
+        int n = (int)s.size();
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            string t = s.substr(i);
+            vector<int> p = prefix_function(t);
+            long long w = 0;
+            for (int x : p) w += x;
+            ans += w;
+        }
+        cout << ans << '\n';
+    }
+}
+
+void solve() {
+    clock_t start, end;
+    start = clock();
+    int T = 1;
+    // cin >> T;
+    int TestCase = 0;
+    while (T--) {
+        TestCase += 1;
+        cerr << "Processing test = " << TestCase << '\n';
+        // cout << "Case #" << TestCase << ": ";
+        sol();
+        // if (T) cout << '\n';
+    }
+    end = clock();
+    cerr << "Time = " << (double)(end - start) / (double)CLOCKS_PER_SEC << '\n';
+}
+
+int main(int argc, char* argv[]) {
+    // srand(time(nullptr));
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    open_file();
+    solve();
+    return 0;
+}
