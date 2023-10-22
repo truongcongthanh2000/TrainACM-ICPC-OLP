688da7690f76a5e873221da10151c054534165a2
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2023-10-22 23:51:09 +0700

    update solution problem A 2023 ICPC Vietnam Central Contest

--- 2023 ICPC Vietnam Central Provincial Contest/A.cpp
+++ 2023 ICPC Vietnam Central Provincial Contest/A.cpp
@@ -1,47 +1,92 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#define ll long long
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
 using namespace std;
 
-void solve()
-{
-  int n;
-  cin >> n;
-  vector<int> m(n, 0);
-  for (int i = 0; i < n; i++)
-  {
-    cin >> m[i];
-  }
+#define INP "input"
+#define OUT "output"
 
-  int max_so_far = INT_MIN, max_ending_here = 0, max_ele = INT_MIN, ans = 0;
-
-  for (int i = 0; i < n; i++)
-  {
-    max_ending_here = max_ending_here + m[i];
-    max_ele = max(max_ele, m[i]);
-
-    if (max_so_far < max_ending_here)
-    {
-      max_so_far = max_ending_here;
-      ans = max_so_far - max_ele;
-    }
-
-    if (max_ending_here < 0)
-    {
-      max_ending_here = 0;
-      max_ele = INT_MIN;
-    }
-  }
-
-  cout << ans;
+/* some template */
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& a) {
+    out << (int)a.size() << '\n';
+    for (const auto& v : a) out << v << ' ';
+    out << endl;
+    return out;
 }
 
-int main()
-{
-  ios_base::sync_with_stdio(0);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<vector<T>>& a) {
+    out << (int)a.size() << '\n';
+    for (const auto& v : a) {
+        for (const auto& value : v) out << value << ' ';
+        out << endl;
+    }
+    return out;
+}
 
-  solve();
+template <typename T>
+std::istream& operator>>(std::istream& is, std::vector<T>& v) {
+    for (auto& x : v) is >> x;
+    return is;
+}
+/* end template */
+
+const long long INF_LL = 1e18;
+const int INF = 1e9 + 100;
+const long double EPS = 1e-6;
+const int BLOCK = 550;
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+mt19937 rnd(chrono::system_clock::now().time_since_epoch().count());
+
+const int maxN = 2e5 + 100;
+const int MOD = 1e9 + 7;
+
+void sol() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cin >> a;
+
+    int ans = 0;
+    for (int max_val = 0; max_val <= 500; max_val++) {
+        int s = 0;
+        for (int x : a) {
+            if (x > max_val) x = -INF;
+            s = max(s + x, x);
+            ans = max(ans, s - max_val);
+            if (s < 0) {
+                s = 0;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
+
+void solve() {
+    clock_t start, end;
+    start = clock();
+    int T = 1;
+    // cin >> T;
+    int TestCase = 0;
+    while (T--) {
+        TestCase += 1;
+        // cerr << "Processing test = " << TestCase << '\n';
+        // cout << "Case #" << TestCase << ": ";
+        sol();
+        // if (T) cout << '\n';
+    }
+    end = clock();
+    cerr << "Time = " << (double)(end - start) / (double)CLOCKS_PER_SEC << '\n';
+}
+
+int main(int argc, char* argv[]) {
+    // srand(time(nullptr));
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
 }
